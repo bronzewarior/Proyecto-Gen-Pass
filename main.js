5b307ddce2b1789a80dbbbf94d54a7e8ba69e369
jmanuel.gh <110192267+jmanueltorress@users.noreply.github.com>
2025-08-07 19:55:00 -0600

    add src

---
+++ main.js
@@ -0,0 +1,3 @@
+//El Codigo JS lo terminaremos de analizar y desarrollar en la clase 5
+//Por ahora tienes  los recursos visuales html y css
+//Recuerda que los archivos html se abren con un navegador web o con live server  o un servidor web local
